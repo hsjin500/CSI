ef3b0b369c5c5cea7ede5b4bbebd1901499b721e
hsjin500 <hsjin500@naver.com>
2020-07-29 11:23:15 +0900

    sql 문장 수정

--- login_process/src/pack1/sql.sql
+++ login_process/src/pack1/sql.sql
@@ -1,17 +1,17 @@
 select * from clients
 
---drop table clients
+drop table clients
 
 create table clients (
-client_id varchar2(12) not null,
-client_pw varchar2(15) not null,
-client_name varchar2(30) not null,
-phone_number varchar2(30) not null,
-client_email varchar2(50) not null,
+client_id varchar(12) not null,
+client_pw varchar(15) not null,
+client_name varchar(30) not null,
+phone_number varchar(30) not null,
+client_email varchar(50) not null,
 signup_date date,
-guard varchar2(3),
-manager varchar2(7),
+guard varchar(3),
+manager varchar(7),
 constraint client_id_pk primary key(client_id)
 )
 
-insert into clients VALUES ('hsjin200', 'hsjin200', '全己柳', '01011112222', 'imsun0101@naver.com',sysdate)
+insert into clients VALUES ('csi1234', 'csi1234', '全己柳', '01011112222', 'imsun0101@naver.com', sysdate, null, null )
